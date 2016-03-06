745bc2b208717a7aebf352294c9acab060a9abaf
Leonardo Di Donato <leodidonato@gmail.com>
2016-03-06 22:12:08 +0100

    ES6 setup files

---
+++ rollup.config.js
@@ -0,0 +1,14 @@
+'use strict'
+
+import babel from 'rollup-plugin-babel';
+
+export default {
+  entry: 'src/index.js',
+  format: 'cjs',
+  plugins: [
+    babel({
+      exclude: 'node_modules/**'
+    })
+  ],
+  dest: 'index.js'
+}
