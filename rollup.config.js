bc6d2414a3208dde865086309a565a3f88ddf79e
Leonardo Di Donato <leodidonato@gmail.com>
2016-04-19 01:22:25 +0200

    Build: Improvements (fixes #3, fixes #4, fixes #6)

--- rollup.config.js
+++ rollup.config.js
@@ -3,10 +3,11 @@
 import babel from 'rollup-plugin-babel';
 
 export default {
-  entry: 'src/index.js',
+  entry: 'index.m.js',
   format: 'cjs',
   plugins: [
     babel({
+      presets: ['es2015-rollup'],
       exclude: 'node_modules/**'
     })
   ],
