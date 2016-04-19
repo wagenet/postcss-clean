8c9993d8e5f7cdc86986d593df5049a6a236ac6f
Leonardo Di Donato <leodidonato@gmail.com>
2016-04-19 14:35:00 +0200

    New: Code coverage plus coveralls integration (fixes #7)

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,25 @@
 'use strict'
 
+/* global process */
+
 import babel from 'rollup-plugin-babel';
 
-export default {
+const inDevelopment = () => process.env.BUILD_ENV && ['development', 'dev', 'develop'].indexOf(process.env.BUILD_ENV.toLowerCase()) >= 0
+
+const babelOpts = {
+  presets: ['es2015-rollup'],
+  exclude: 'node_modules/**'
+}
+
+const rollupOpts = {
   entry: 'index.m.js',
   format: 'cjs',
-  plugins: [
-    babel({
-      presets: ['es2015-rollup'],
-      exclude: 'node_modules/**'
-    })
-  ],
+  plugins: [ babel(babelOpts) ],
   dest: 'index.js'
 }
+
+if (inDevelopment()) {
+  rollupOpts.sourceMap = 'inline'
+}
+
+export default rollupOpts
