1e4765f1c3157e34edf93b328c5adfb8d039cd33
leodido <leodidonato@gmail.com>
2017-07-06 01:21:37 +0200

    Build: Destructuring within the configuration file not possible for node <= 5

--- rollup.config.js
+++ rollup.config.js
@@ -3,27 +3,24 @@
 import buble from 'rollup-plugin-buble'
 import { readFileSync } from 'fs';
 
-function inDevelopment() {
-  return process.env.BUILD_ENV && ['development', 'dev', 'develop'].indexOf(process.env.BUILD_ENV.toLowerCase()) >= 0
-}
+const inDevelopment = () =>
+  process.env.BUILD_ENV &&
+  ['development', 'dev', 'develop'].indexOf(process.env.BUILD_ENV.toLowerCase()) >= 0
 
-const {
-  main: dst,
-  module: src,
-  dependencies: dep
-} = JSON.parse(readFileSync('./package.json', 'utf-8'))
+
+const pkg = JSON.parse(readFileSync('./package.json', 'utf-8'))
 
 const rollupOpts = {
-  entry: src,
+  entry: pkg.module,
   format: 'cjs',
-  external: Object.keys(dep),
+  external: Object.keys(pkg.dependencies),
   plugins: [
     buble({
-      include: src,
+      include: pkg.module,
       transforms: { dangerousForOf: true }
     })
   ],
-  dest: dst
+  dest: pkg.main
 }
 
 if (inDevelopment()) {
